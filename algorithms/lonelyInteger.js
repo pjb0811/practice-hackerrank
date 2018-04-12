9f333f004e333a411754f77a6473b116684ca7ef
jbpark <jbpark@wemakeprice.com>
2018-04-12 12:18:27 +0900

    update practice functions

--- algorithms/lonelyInteger.js
+++ algorithms/lonelyInteger.js
@@ -1,16 +1,25 @@
 function lonelyInteger(arr) {
-  const temp = [];
-  const result = arr.reduce((prev, curr) => {
+  let result = '';
+  const arr1 = [];
+  const arr2 = arr.reduce((prev, curr) => {
     if (prev.indexOf(curr) < 0 ) {
       prev.push(curr);
     } else {
-      temp.push(curr);
+      arr1.push(curr);
     }
     return prev;
   }, []);
 
-  console.log(temp);
+  arr2.map((value) => {
+    const found = arr1.find((num) => {
+      return num === value;
+    });
+    if (typeof found === 'undefined') {
+      result = value;
+    }
+  });
+
   return result;
 }
 
-console.log(lonelyInteger('0 0 1 2 1'.split(' ')));
+console.log(lonelyInteger('0 0 1 2 2 3 3 4 4'.split(' ')));
